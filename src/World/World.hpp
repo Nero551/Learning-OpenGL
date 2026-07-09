32b3495a36d3e38286a9a5e46ef75f1de7ba32d0
Nero <109078102+Nero551@users.noreply.github.com>
2026-07-09 13:50:54 +0300

    creating basis for ECS, entity and components done so far

--- src/World/World.hpp
+++ src/World/World.hpp
@@ -1,8 +1,27 @@
 #pragma once
-#include "Object.hpp"
+#include "Entity.hpp"
 #include <unordered_map>
+#include <concepts>
+
+template <typename T>
+concept EntityType = std::derived_from<T, Entity>;
 
 struct World {
   // TODO- this guy should own runtime world state (stuff that exist).
-  std::unordered_map<unsigned int, Object> Entities;
+  std::unordered_map<unsigned int, Entity> Entities;
+
+  template <EntityType T> void CreateEntity() {
+    if (id >= max) {
+      return;
+    }
+
+    T entity(id);
+    Entities.insert({id, entity});
+
+    id++;
+  }
+
+private:
+  unsigned int id = 0;
+  unsigned int max = 10000;
 };
