5280dc267f73ace7ced4327f9dab0a3ebd86eab1
Nero <109078102+Nero551@users.noreply.github.com>
2026-07-10 08:13:24 +0300

    fixed camera. the GetRight() and GetUp() methods had their cross reversed. also the ViewMatrix wasn't taking Position into the direction calculation

--- src/Modules/Renderer/Renderer.cpp
+++ src/Modules/Renderer/Renderer.cpp
@@ -5,16 +5,23 @@ void Renderer::Start() {}
 
 void Renderer::Update(double dt) {}
 
+Matrix4 CreateViewMatrix() {
+  auto camera = Engine::Instance->World.ActiveScene->ActiveCamera;
+
+  Vector3 pos = camera->GetComponent<TransformComponent>().Position;
+  Vector3 forward = camera->GetComponent<TransformComponent>().GetForward();
+  Vector3 up = camera->GetComponent<TransformComponent>().GetUp();
+
+  Matrix4 view = Matrix4::LookAt(pos, pos + forward, up);
+
+  return view;
+}
+
 void Renderer::Render() {
   auto camera = Engine::Instance->World.ActiveScene->ActiveCamera;
 
   Matrix4 projection = camera->GetComponent<CameraComponent>().GetProjectionMatrix();
-
-  Vector3 pos = camera->GetComponent<TransformComponent>().Position;
-  Vector3 forward = camera->GetComponent<TransformComponent>().Forward;
-  Vector3 up = camera->GetComponent<TransformComponent>().GetUp();
-
-  Matrix4 view = Matrix4::LookAt(pos, forward, up);
+  Matrix4 view = CreateViewMatrix();
 
   for (Object object : Objects) {
     object.Draw(view, projection);
