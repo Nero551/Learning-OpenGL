c3806a285b75f458c7565e6efbbda9e3564f12d3
Nero <109078102+Nero551@users.noreply.github.com>
2026-07-13 16:28:13 +0300

    made a new GitHooks folder for the "run tests before commit" hook

---
+++ GitHooks/pre-commit.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#
+#echo "Running unit tests..."
+#
+#./build/src/Tests/UnitTests
+#
+#if [ $? -ne 0 ]; then
+#    echo "❌ Unit tests failed. Commit aborted."
+#    exit 1
+#fi
+#
+#echo "✅ All tests passed."
+#exit 0
