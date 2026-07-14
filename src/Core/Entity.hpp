a857fba42b07e283664287c2f4ed3e5b55613bf1
Nero <109078102+Nero551@users.noreply.github.com>
2026-07-14 16:29:33 +0300

    added check in AddComponent(). now if the component already exists , it shows an error but still continues while returning the existing component

--- src/Core/Entity.hpp
+++ src/Core/Entity.hpp
@@ -4,61 +4,74 @@
 #include <concepts>
 #include <memory>
 #include <format>
-#include <ranges>
 #include <typeindex>
 #include <unordered_map>
 
-template<typename T>concept ComponentType = std::derived_from<T, Component>;
+#include "Utilities/Services/LoggerService.hpp"
 
-struct Entity {
+template <typename T>concept ComponentType = std::derived_from<T, Component>;
+
+struct Entity
+{
    unsigned int Id = 0;
 
-   virtual ~Entity() {}
+   virtual ~Entity() = default;
 
    Entity() = default;
 
-   Entity(const Entity &) = delete;
+   Entity(const Entity&) = delete;
 
-   Entity &operator=(const Entity &) = delete;
+   Entity& operator=(const Entity&) = delete;
 
-   Entity(Entity &&) = default;
+   Entity(Entity&&) = default;
 
-   Entity &operator=(Entity &&) = default;
+   Entity& operator=(Entity&&) = default;
 
    virtual void Initialize() {}
 
-   template<ComponentType T> T &AddComponent() {
+   template <ComponentType T> T& AddComponent()
+   {
+      if (Components.contains(typeid(T)))
+      {
+         LoggerService::Error(std::format("Entity {} already contains component {}", Id, typeid(T).name()));
+         return static_cast<T&>((*Components.at(typeid(T))));
+      }
+
       auto component = std::make_unique<T>();
 
-      T &ref = *component;
+      T& ref = *component;
 
       Components.emplace(typeid(T), std::move(component));
       return ref;
    }
 
-   template<ComponentType T> T &GetComponent() {
+   template <ComponentType T> T& GetComponent()
+   {
       auto component = Components.find(typeid(T));
-      if (component == Components.end()) {
-         throw std::runtime_error(std::format("Component Not Found: {}", typeid(T).name()));
+      if (component == Components.end())
+      {
+         LoggerService::Fatal(std::format("Component Not Found: {}", typeid(T).name()));
       }
-      return static_cast<T &>((*component->second));
+      return static_cast<T&>((*component->second));
    }
 
-   template<ComponentType T> bool HasComponent() {
+   template <ComponentType T> bool HasComponent()
+   {
       auto component = Components.find(typeid(T));
       return component != Components.end();
    }
 
-   // template<ComponentType T> T &CopyComponent(const T &otherComponent) {
-   //    auto component = std::make_unique<T>(otherComponent);
-   //
-   //    T *ptr = component.get();
-   //
-   //    Components.insert_or_assign(typeid(T), std::move(component));
-   //
-   //    return *ptr;
-   // }
+   template <ComponentType T> T& CopyComponent(const T& otherComponent)
+   {
+      auto component = std::make_unique<T>(otherComponent);
+
+      T* ptr = component.get();
+
+      Components.insert_or_assign(typeid(T), std::move(component));
+
+      return *ptr;
+   }
 
 private:
-   std::unordered_map<std::type_index, std::unique_ptr<Component> > Components;
+   std::unordered_map<std::type_index, std::unique_ptr<Component>> Components;
 };
